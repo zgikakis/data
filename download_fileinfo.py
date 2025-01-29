f2994615f70cdfb622d5ea0c0b19bf0e86b51b4a
Zach Gikakis <zmgikakis@gmail.com>
2025-01-29 09:48:35 -0500

    Update download_fileinfo.py
    create df from sample info

--- download_fileinfo.py
+++ download_fileinfo.py
@@ -1,6 +1,8 @@
 # 1) Creates a pandas dataframe containing information from a usaspending API bulk download POST request
 # 2) Downloads df to current directory as a csv
 
+import pandas as pd
+
 start_date = "2024-12-01"
 end_date = "2024-12-31"
 status_url = "https://api.usaspending.gov/api/v2/download/status?file_name=All_PrimeTransactions_2025-01-29_H14M09S08664008.zip"
@@ -11,3 +13,18 @@ file_url = "https://files.usaspending.gov/generated_downloads/All_PrimeTransacti
 infofile = ('prime_ct_{}_{}'.format(start_date, end_date)).replace('-', "")
 
 print(infofile)
+
+infodata = {'infofile':[infofile],
+            'start_date':[start_date],
+            'end_date':[end_date],
+            'status_url':[status_url],
+            'filename':[filename],
+            'file_url':[file_url]}
+
+info_df = pd.DataFrame(infodata)
+
+# Set option to display all columns and rows
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+print(info_df)
