6e86d3bf330fa8656333fc146b8957be7b1b8fec
Zach Gikakis <zmgikakis@gmail.com>
2025-01-29 09:52:05 -0500

    Update download_fileinfo.py

--- download_fileinfo.py
+++ download_fileinfo.py
@@ -9,11 +9,9 @@ status_url = "https://api.usaspending.gov/api/v2/download/status?file_name=All_P
 filename = "All_PrimeTransactions_2025-01-29_H14M09S08664008.zip"
 file_url = "https://files.usaspending.gov/generated_downloads/All_PrimeTransactions_2025-01-29_H14M09S08664008.zip"
 
-
+# Create and format infofile, which will serve as the filename for the csv file
 infofile = ('prime_ct_{}_{}'.format(start_date, end_date)).replace('-', "")
-
-print(infofile)
-
+# Create dictionary for use in dataframe creation
 infodata = {'infofile':[infofile],
             'start_date':[start_date],
             'end_date':[end_date],
@@ -21,6 +19,7 @@ infodata = {'infofile':[infofile],
             'filename':[filename],
             'file_url':[file_url]}
 
+# Create dataframe containing info from USA spending bulk download POST request
 info_df = pd.DataFrame(infodata)
 
 # Set option to display all columns and rows
